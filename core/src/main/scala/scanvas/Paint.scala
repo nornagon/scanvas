2ef30e43bf2201e0f7a86e4540f876705c57e626
Jeremy Apthorp <nornagon@nornagon.net>
2018-03-09 14:09:38 -0800

    Some simple line wrapping algorithms

--- core/src/main/scala/scanvas/Paint.scala
+++ core/src/main/scala/scanvas/Paint.scala
@@ -1,10 +1,13 @@
 package scanvas
 
 import java.nio.ByteBuffer
+import java.text.BreakIterator
 
-import org.bytedeco.javacpp.Pointer
 import org.bytedeco.javacpp.Skia._
+import org.bytedeco.javacpp.{BytePointer, Pointer}
 import scanvas.Paint.FontMetrics
+
+import scala.collection.mutable
 
 class Paint private[scanvas] (private[scanvas] val p: sk_paint_t) {
   def setAntiAlias(b: Boolean): Paint = { sk_paint_set_antialias(p, b); this }
@@ -21,10 +24,72 @@ class Paint private[scanvas] (private[scanvas] val p: sk_paint_t) {
   def setTypeface(tf: Typeface): Paint = { sk_paint_set_typeface(p, tf.tf); this }
   def setMaskFilter(mf: MaskFilter): Paint = { sk_paint_set_maskfilter(p, mf.mf); this }
 
+  // TODO(jeremy): other functions use top/left/width/height. standardize.
   def getTextBounds(text: String): (Float, Float, Float, Float) = {
     val textBytes = new Pointer(ByteBuffer.wrap(text.getBytes))
     sk_paint_measure_text(p, textBytes, text.getBytes.length, Paint.tmpRect)
     (Paint.tmpRect.top(), Paint.tmpRect.left(), Paint.tmpRect.bottom(), Paint.tmpRect.right())
+  }
+
+  def breakText(text: String, maxWidth: Float): (Long, Float) = {
+    val textBytes = new BytePointer(text, "UTF-8")
+    val measuredWidth = new Array[Float](1)
+    val measuredBytes = sk_paint_break_text(p, textBytes, text.getBytes.length, maxWidth, measuredWidth)
+    (measuredBytes, measuredWidth(0))
+  }
+
+  def wrapText(text: String, maxWidth: Float): (Seq[String], Float) = {
+    val textBytes = new BytePointer(text, "UTF-8")
+    val measuredWidth = new Array[Float](1)
+    var pos = 0l
+    val lines = mutable.ArrayBuffer.empty[String]
+    var maxMeasuredWidth = 0f
+
+    while (pos < textBytes.limit()) {
+      val measuredBytes = sk_paint_break_text(p, textBytes, textBytes.limit() - pos, maxWidth, measuredWidth)
+      lines += new BytePointer(textBytes).limit(pos + measuredBytes).getString("UTF-8")
+      pos += measuredBytes
+      textBytes.position(pos)
+      if (measuredWidth(0) > maxMeasuredWidth) {
+        maxMeasuredWidth = measuredWidth(0)
+      }
+    }
+    (lines, maxMeasuredWidth)
+  }
+
+  // TODO: this is starting to get a bit ridiculous
+  def wrapText2(text: String, maxWidth: Float): (Seq[String], Float) = {
+    var maxLineWidth = 0f
+    val lines = mutable.ArrayBuffer.empty[String]
+    val wordIterator = BreakIterator.getLineInstance
+    wordIterator.setText(text)
+    var start = wordIterator.first
+    var end = wordIterator.next
+    val line = StringBuilder.newBuilder
+    var lineWidth = 0f
+    while (end != BreakIterator.DONE) {
+      val word = text.substring(start, end)
+      val textBytes = new BytePointer(word, "UTF-8")
+      val width = sk_paint_measure_text(p, textBytes, textBytes.limit(), null)
+      if (lineWidth + width > maxWidth) {
+        // break the line
+        lines.append(line.toString())
+        line.clear()
+        lineWidth = width
+      } else {
+        lineWidth += width
+      }
+      line.append(word)
+      if (lineWidth > maxLineWidth)
+        maxLineWidth = lineWidth
+
+      start = end
+      end = wordIterator.next
+    }
+    if (line.nonEmpty) {
+      lines.append(line.toString())
+    }
+    (lines, maxLineWidth)
   }
 
   def fontMetrics(): FontMetrics = {
@@ -56,7 +121,9 @@ object Paint {
     capHeight: Float,
     underlineThickness: Float,
     underlinePosition: Float
-  )
+  ) {
+    lazy val lineSpacing: Float = descent - ascent + leading
+  }
 
   object FontMetrics {
     private[scanvas] def fromSkFontMetrics(fm: sk_fontmetrics_t): FontMetrics = {
